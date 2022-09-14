9db009a774eccd9fd8e11cf05b5d61d6f02cd737
Simon Platz <55438543+simonplatz@users.noreply.github.com>
2022-09-14 12:28:26 +0200

    Update example.py

--- lecture02/parquet-word-count/example.py
+++ lecture02/parquet-word-count/example.py
@@ -1,3 +1,5 @@
+import encodings
+from tkinter.tix import COLUMN
 from hdfs import InsecureClient
 from collections import Counter
 import pyarrow.parquet as pq
@@ -12,5 +14,12 @@ wordcount = None
 with client.read('/alice-in-wonderland.txt', encoding='utf-8') as reader:
     wordcount = Counter(reader.read().split()).most_common(10)
 
-    
+dataframe = pd.DataFrame(wordcount, columns=['word', 'columns'])
+
+
 # To-Do: Save the wordcount in a Parquet file and read it again!
+table = pa.Table.from_pandas(dataframe)
+
+client.write('/wordcount.parquet', encoding='utf-8')
+
+readTable = pq.read_table('/wordcount.parquet')
